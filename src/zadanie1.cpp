0cd598f2f3a25b76037e52e2064cdbc2b8681b79
Kacper Machlus <91918721+GrizzlyBeats@users.noreply.github.com>
2021-10-26 14:36:07 +0200

    Add files via upload

---
+++ src/zadanie1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+
+
+int main()
+{
+    std::string pass ="student";
+    std::string x="";
+    do
+    {
+        std::cout<<"Password:";
+        std::getline(std::cin,x);
+    
+    
+    }
+    while(pass!= x);
+    std::cout<<"ok!\n";
+
+    return 0;
+}
