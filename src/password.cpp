6e0129daf8381d1bb53af06f383bb055ceb00723
Kacper Machlus <91918721+GrizzlyBeats@users.noreply.github.com>
2021-10-29 09:24:13 +0200

    Add files via upload

---
+++ src/password.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+auto main(int argc, char* argv[])-> int{
+    if (argc == 1) {
+        return 1;
+    }
+   
+    std::string password=argv[1],haslo;
+    do{
+        std::cout << "password: ";
+        std::cin >> haslo;
+    }
+    while(password!=haslo);
+    std::cout << "ok!\n";
+
+    return 0;
+}
